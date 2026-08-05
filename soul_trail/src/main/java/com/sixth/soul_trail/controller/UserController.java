062824cc93a83bb8619f5831b961bda8b3f58581
CodeFort_CTY <3809533918@qq.com>
2026-08-05 15:29:49 +0800

    register_modify4
    用户注册接口的完整业务逻辑统一封装于用户模块的控制器层、业务服务层与数据访问层

--- soul_trail/src/main/java/com/sixth/soul_trail/controller/UserController.java
+++ soul_trail/src/main/java/com/sixth/soul_trail/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
-public class RegisterController {
+public class UserController {
 
     @Autowired
     private UserService userService;
